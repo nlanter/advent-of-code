8a36c968af13f9d5b6f59853c975c527f8584479
Emery <memery@figure.com>
2022-12-01 08:08:14 -0700

    2022-12-01

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
@@ -2,6 +2,14 @@ package org.elwaxoro.advent.y2022
 
 import org.elwaxoro.advent.PuzzleDayTester
 
-//class Dec01: PuzzleDayTester(1, 2022) {
-//
-//}
+/**
+ * Day 1: Calorie Counting
+ */
+class Dec01: PuzzleDayTester(1, 2022) {
+
+    override fun part1(): Any = loader().last()// == 70374
+
+    override fun part2(): Any = loader().takeLast(3).sum()// == 204610
+
+    private fun loader(): List<Int> = load(delimiter = "\n\n").map { it.split("\n").sumOf(String::toInt) }.sorted()
+}
