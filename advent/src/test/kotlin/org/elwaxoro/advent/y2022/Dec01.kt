afdc850b7ae29b7ddc5b347c08a856b84450dd0c
nlanter <nlanter@figure.com>
2022-12-02 10:36:55 -0500

    day 1 part one

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
@@ -2,6 +2,8 @@ package org.elwaxoro.advent.y2022
 
 import org.elwaxoro.advent.PuzzleDayTester
 
-//class Dec01: PuzzleDayTester(1, 2022) {
-//
-//}
+class Dec01: PuzzleDayTester(1, 2022) {
+
+    override fun part1(): Any = load(delimiter = "\n\n").map { it.split("\n") }.map { elf -> elf.map { it.toInt() } }.maxOf { it.sum() }
+
+}
