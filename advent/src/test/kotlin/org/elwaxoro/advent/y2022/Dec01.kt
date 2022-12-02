3990d7616616a2fdb62947ec8c4164600fa1aa22
nlanter <nlanter@figure.com>
2022-12-02 10:39:09 -0500

    Update Dec01.kt

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
@@ -4,6 +4,6 @@ import org.elwaxoro.advent.PuzzleDayTester
 
 class Dec01: PuzzleDayTester(1, 2022) {
 
-    override fun part1(): Any = load(delimiter = "\n\n").map { it.split("\n") }.map { elf -> elf.map { it.toInt() } }.maxOf { it.sum() }
+    override fun part1(): Any = load(delimiter = "\n\n").map { elfValues ->  elfValues.split("\n").map { calories -> calories.toInt() } }.maxOf { it.sum() }
 
 }
