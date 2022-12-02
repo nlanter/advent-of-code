174d43ca4b3f21ae76e718acd1a7eab8a9bbcef0
nlanter <nlanter@figure.com>
2022-12-02 12:17:56 -0500

    Update Dec02.kt

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec02.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec02.kt
@@ -2,18 +2,20 @@ package org.elwaxoro.advent.y2022
 
 import org.elwaxoro.advent.PuzzleDayTester
 
-class Dec02: PuzzleDayTester(2, 2022) {
+class Dec02 : PuzzleDayTester(2, 2022) {
 
     override fun part1(): Any {
         var opponentScore = 0
         var myScore = 0
 
         load().forEach {
-            val (opp, me) = it.split(" ").let{ plays -> RPS.fromString(plays[0]) to RPS.fromString(plays[1])}
+            val (opp, me) = it.split(" ").let { plays -> RPS.fromString(plays[0]) to RPS.fromString(plays[1]) }
             when {
                 opp.beats(me) -> opponentScore += winningScore
                 me.beats(opp) -> myScore += winningScore
-                else -> {opponentScore += drawScore; myScore += drawScore}
+                else -> {
+                    opponentScore += drawScore; myScore += drawScore
+                }
             }
 
             opponentScore += opp.playScore
@@ -57,10 +59,18 @@ class Dec02: PuzzleDayTester(2, 2022) {
         var myScore = 0
 
         load().forEach {
-            val (opp, winLoseOrDraw) = it.split(" ").let{ plays -> RPSV2.fromString(plays[0]) to WinLoseOrDraw.fromString(plays[1])}
+            val (opp, winLoseOrDraw) = it.split(" ")
+                .let { plays -> RPSV2.fromString(plays[0]) to WinLoseOrDraw.fromString(plays[1]) }
 
             val score = getMyPlay(winLoseOrDraw, opp).playScore + winLoseOrDraw.playScore
-            println("enemy played $opp, i played ${getMyPlay(winLoseOrDraw, opp)} and $winLoseOrDraw with a score of $score")
+            println(
+                "enemy played $opp, i played ${
+                    getMyPlay(
+                        winLoseOrDraw,
+                        opp
+                    )
+                } and $winLoseOrDraw with a score of $score"
+            )
             myScore += score
         }
 
@@ -76,27 +86,25 @@ class Dec02: PuzzleDayTester(2, 2022) {
     }
 
     enum class RPSV2(private val opponentPlay: String, val playScore: Int) {
-        ROCK("A",  1),
-        PAPER("B", 2),
-        SCISSORS("C",  3),
-        IGNORED("",  0);
+        ROCK("A", 1) {
+            override fun beats(): RPSV2 = SCISSORS
+            override fun losesTo(): RPSV2 = PAPER
+        },
+        PAPER("B", 2) {
+            override fun beats(): RPSV2 = ROCK
+            override fun losesTo(): RPSV2 = SCISSORS
+        },
+        SCISSORS("C", 3) {
+            override fun beats(): RPSV2 = PAPER
+            override fun losesTo(): RPSV2 = ROCK
+        },
+        IGNORED("", 0) {
+            override fun beats(): RPSV2 = IGNORED
+            override fun losesTo(): RPSV2 = IGNORED
+        };
 
-        fun beats(): RPSV2 {
-            return when {
-                this == ROCK -> SCISSORS
-                this == PAPER -> ROCK
-                this == SCISSORS -> PAPER
-                else -> IGNORED
-            }
-        }
-        fun losesTo(): RPSV2 {
-            return when {
-                this == ROCK -> PAPER
-                this == PAPER -> SCISSORS
-                this == SCISSORS -> ROCK
-                else -> IGNORED
-            }
-        }
+        abstract fun beats(): RPSV2
+        abstract fun losesTo(): RPSV2
 
         companion object {
             fun fromString(str: String): RPSV2 {
