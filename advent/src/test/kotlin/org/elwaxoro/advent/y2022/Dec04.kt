f71aa1b4fe9c6472db00cf549fabe175bb142659
Emery <memery@figure.com>
2022-12-04 09:21:13 -0700

    2022-12-04

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec04.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec04.kt
@@ -2,14 +2,21 @@ package org.elwaxoro.advent.y2022
 
 import org.elwaxoro.advent.PuzzleDayTester
 
+/**
+ * Day 4: Camp Cleanup
+ */
 class Dec04: PuzzleDayTester(4, 2022) {
 
+    /**
+     * One range completely overlaps another range if their intersection is exactly the size of the smaller range
+     */
     override fun part1(): Any = rangeLoader().count { (a, b) ->
-        a.intersect(b).size.let {
-            it == a.count() || it == b.count()
-        }
+        a.intersect(b).size == minOf(a.count(), b.count())
     }// == 651
 
+    /**
+     * Partial overlap is just a non-empty intersection
+     */
     override fun part2(): Any = rangeLoader().count { (a, b) ->
         a.intersect(b).isNotEmpty()
     }// == 956
