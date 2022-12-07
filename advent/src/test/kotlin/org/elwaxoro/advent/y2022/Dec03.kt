1a4bce57d7e5a7848ab90c4c432ee5e06d51441d
nlanter <nlanter@figure.com>
2022-12-07 10:15:58 -0500

    p2

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec03.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec03.kt
@@ -12,5 +12,10 @@ class Dec03 : PuzzleDayTester(3, 2022) {
         return load().map { it.substring(0, it.length / 2) to it.substring(it.length / 2, it.length) }
     }
 
+    override fun part2(): Any = loadData2().map { list -> list.first().filter { c -> list[1].contains(c) && list[2].contains(c) }.toSet().first().toInt() }
+        .sumOf { if (it > 90) it - 96 else it - 38 }
+
+    fun loadData2(): List<List<String>> = load().windowed(3, step = 3)
+
 
 }
