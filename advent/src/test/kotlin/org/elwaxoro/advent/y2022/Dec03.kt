aaf04136a24ebcb928aadd3a37cc778439f32d27
nlanter <nlanter@figure.com>
2022-12-07 10:03:55 -0500

    Update Dec03.kt

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec03.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec03.kt
@@ -4,7 +4,9 @@ import org.elwaxoro.advent.PuzzleDayTester
 
 class Dec03 : PuzzleDayTester(3, 2022) {
 
-    override fun part1(): Any = loadData().map {pair -> pair.first.filter { c -> pair.second.contains(c) }.toSet().first().toInt()}.map {if (it > 90) it - 96 else it - 38}.sum()
+    override fun part1(): Any =
+        loadData().map { pair -> pair.first.filter { c -> pair.second.contains(c) }.toSet().first().toInt() }
+            .sumOf { if (it > 90) it - 96 else it - 38 }
 
     fun loadData(): List<Pair<String, String>> {
         return load().map { it.substring(0, it.length / 2) to it.substring(it.length / 2, it.length) }
