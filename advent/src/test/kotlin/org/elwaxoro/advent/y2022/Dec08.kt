a812b9d1130d14faff51eaad37a081f714506c23
nlanter <nlanter@figure.com>
2022-12-08 16:13:39 -0500

    Update Dec08.kt

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec08.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec08.kt
@@ -10,10 +10,10 @@ class Dec08 : PuzzleDayTester(8, 2022) {
         var visibleCount = 0
         grid.forEachIndexed { rowIdx, row ->
             row.forEachIndexed { colIdx, _ ->
-                if (searchLeft(grid, rowIdx, colIdx)
-                    || searchRight(grid, rowIdx, colIdx)
-                    || searchUp(grid, rowIdx, colIdx)
-                    || searchDown(grid, rowIdx, colIdx)
+                if (searchLeftV2(grid, rowIdx, colIdx).second
+                    || searchRightV2(grid, rowIdx, colIdx).second
+                    || searchUpV2(grid, rowIdx, colIdx).second
+                    || searchDownV2(grid, rowIdx, colIdx).second
                 ) visibleCount += 1
             }
         }
@@ -26,58 +26,11 @@ class Dec08 : PuzzleDayTester(8, 2022) {
         var maxVisibleCount = 0
         grid.forEachIndexed { rowIdx, row ->
             row.forEachIndexed { colIdx, _ ->
-                val visibility = searchLeftV2(grid, rowIdx, colIdx) * searchRightV2(grid, rowIdx, colIdx) * searchUpV2(
-                    grid,
-                    rowIdx,
-                    colIdx
-                ) * searchDownV2(grid, rowIdx, colIdx)
+                val visibility = searchLeftV2(grid, rowIdx, colIdx).first * searchRightV2(grid, rowIdx, colIdx).first * searchUpV2(grid, rowIdx, colIdx).first * searchDownV2(grid, rowIdx, colIdx).first
                 if (visibility > maxVisibleCount) maxVisibleCount = visibility
             }
         }
         return maxVisibleCount
-    }
-
-    private fun searchLeft(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Boolean {
-        var colIdxMutable = colIdx
-        while (colIdxMutable > 0) {
-            if (grid[rowIdx][colIdxMutable - 1] >= grid[rowIdx][colIdx]) {
-                return false
-            }
-            colIdxMutable--
-        }
-        return true
-    }
-    private fun searchRight(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Boolean {
-        var colIdxMutable = colIdx
-        while (colIdxMutable < grid[rowIdx].size - 1) {
-            if (grid[rowIdx][colIdxMutable + 1] >= grid[rowIdx][colIdx]) {
-                return false
-            }
-            colIdxMutable++
-        }
-        return true
-    }
-
-    private fun searchUp(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Boolean {
-        var rowIdxMutable = rowIdx
-        while (rowIdxMutable > 0) {
-            if (grid[rowIdxMutable - 1][colIdx] >= grid[rowIdx][colIdx]) {
-                return false
-            }
-            rowIdxMutable--
-        }
-        return true
-    }
-
-    private fun searchDown(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Boolean {
-        var rowIdxMutable = rowIdx
-        while (rowIdxMutable < grid.size - 1) {
-            if (grid[rowIdxMutable + 1][colIdx] >= grid[rowIdx][colIdx]) {
-                return false
-            }
-            rowIdxMutable++
-        }
-        return true
     }
 
     private fun loadTreesIntoGrid(): Array<IntArray> = load().let { rows -> Array(rows.size) {IntArray(size = rows[0].length)} to rows }.let { (grid, rows) ->
@@ -94,57 +47,57 @@ class Dec08 : PuzzleDayTester(8, 2022) {
 
 
 
-    private fun searchLeftV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Int {
+    private fun searchLeftV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Pair<Int, Boolean> {
         var colIdxMutable = colIdx
         var viewingDistance = 0
         while (colIdxMutable > 0) {
             if (grid[rowIdx][colIdxMutable - 1] >= grid[rowIdx][colIdx]) {
-                return viewingDistance + 1
+                return viewingDistance + 1 to false
             }
             colIdxMutable--
             viewingDistance++
         }
-        return viewingDistance
+        return viewingDistance to true
     }
-    private fun searchRightV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Int {
+    private fun searchRightV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Pair<Int, Boolean> {
         var colIdxMutable = colIdx
         var viewingDistance = 0
         while (colIdxMutable < grid[rowIdx].size - 1) {
             if (grid[rowIdx][colIdxMutable + 1] >= grid[rowIdx][colIdx]) {
-                return viewingDistance + 1
+                return viewingDistance + 1 to false
             }
             colIdxMutable++
             viewingDistance++
         }
-        return viewingDistance
+        return viewingDistance to true
     }
 
-    private fun searchUpV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Int {
+    private fun searchUpV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Pair<Int, Boolean> {
         var rowIdxMutable = rowIdx
         var viewingDistance = 0
 
         while (rowIdxMutable > 0) {
             if (grid[rowIdxMutable - 1][colIdx] >= grid[rowIdx][colIdx]) {
-                return viewingDistance + 1
+                return viewingDistance + 1 to false
             }
             rowIdxMutable--
             viewingDistance++
         }
-        return viewingDistance
+        return viewingDistance to true
     }
 
-    private fun searchDownV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Int {
+    private fun searchDownV2(grid: Array<IntArray>, rowIdx: Int, colIdx: Int): Pair<Int, Boolean> {
         var rowIdxMutable = rowIdx
         var viewingDistance = 0
 
         while (rowIdxMutable < grid.size - 1) {
             if (grid[rowIdxMutable + 1][colIdx] >= grid[rowIdx][colIdx]) {
-                return viewingDistance + 1
+                return viewingDistance + 1 to false
             }
             rowIdxMutable++
             viewingDistance++
         }
-        return viewingDistance
+        return viewingDistance to true
     }
 
 
