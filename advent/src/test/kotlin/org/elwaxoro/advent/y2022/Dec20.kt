a130d247907709290e1d0328bc80d6832b70c797
nlanter <nlanter@figure.com>
2022-12-21 10:05:46 -0500

    Update Dec20.kt

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec20.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec20.kt
@@ -3,41 +3,46 @@ package org.elwaxoro.advent.y2022
 import org.elwaxoro.advent.PuzzleDayTester
 
 class Dec20 : PuzzleDayTester(20, 2022) { //11073
-    data class Thingy(val value: Int, val ogIdx: Int) {} // ogIdx to preserve uniqueness when finding current idx of value
+    data class Thingy(
+        val value: Int,
+        val ogIdx: Int
+    ) {} // ogIdx to preserve uniqueness when finding current idx of value
 
-    override fun part1(): Any = loadToInt().mapIndexed { idx, value -> Thingy(value, idx) }
-        .let {
-            it to it.toMutableList()
-        }.let { (original, mutable) ->
-            original.onEach { move(mutable, it) }
-            listOf(1000, 2000, 3000)
+    override fun part1(): Any = CircularList(loadToInt()).run()
+    
+    data class CircularList(val originalList: List<Int>) {
+        private val originalthingyList: MutableList<Thingy> =
+            originalList.mapIndexed { idx, value -> Thingy(value, idx) }.toMutableList()
+        private val thingyList: MutableList<Thingy> = originalthingyList.toMutableList()
+
+        fun run(): Int {
+            originalthingyList.onEach { move(thingyList, it) }
+            return listOf(1000, 2000, 3000)
                 .sumOf {
-                    val idx = (mutable.indexOfFirst { it.value == 0 } + it) % mutable.size
-//                println("$it to ${mutable[idx]}")
-                    mutable[idx].value
+                    thingyList[(thingyList.indexOfFirst { it.value == 0 } + it) % thingyList.size].value
                 }
         }
 
-    fun move(list: MutableList<Thingy>, thingy: Thingy) {
-        val numToMove = thingy.value
-        if (numToMove == 0) {
-            println("0 does not move"); return
-        }
-        val idxOfNum = list.indexOf(thingy)
-        val targetIdx = idxOfNum + numToMove
-        val target = when {
-            targetIdx > list.lastIndex -> targetIdx % list.lastIndex // goes past end, mod to remove times wrapped around
-            targetIdx < 0 -> (targetIdx % list.lastIndex) + list.lastIndex // goes past beginning
-            else -> targetIdx
-        }
+        fun move(list: MutableList<Thingy>, thingy: Thingy) {
+            val numToMove = thingy.value
+            if (numToMove == 0) {
+                println("0 does not move"); return
+            }
+            val idxOfNum = list.indexOf(thingy)
+            val targetIdx = idxOfNum + numToMove
+            val target = when {
+                targetIdx > list.lastIndex -> targetIdx % list.lastIndex // goes past end, mod to remove times wrapped around
+                targetIdx < 0 -> (targetIdx % list.lastIndex) + list.lastIndex // goes past beginning
+                else -> targetIdx
+            }
 
 //        println("moving $numToMove from $idxOfNum to $target")
-        swap(list, idxOfNum, target)
-    }
+            swap(list, idxOfNum, target)
+        }
 
-    private fun swap(list: MutableList<Thingy>, idxOfNum: Int, targetIdx: Int) {
-        val v = list.removeAt(idxOfNum)
-        list.add(targetIdx, v)
+        private fun swap(list: MutableList<Thingy>, idxOfNum: Int, targetIdx: Int) {
+            val v = list.removeAt(idxOfNum)
+            list.add(targetIdx, v)
+        }
     }
-
 }
