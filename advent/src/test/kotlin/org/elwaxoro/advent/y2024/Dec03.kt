191e875af7383e296368e73a00cd34b76c06b9ff
nlanter <nlanter@figure.com>
2024-12-04 13:07:23 -0500

    part 2

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec03.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec03.kt
@@ -5,7 +5,18 @@ import kotlin.math.absoluteValue
 
 class Dec3: PuzzleDayTester(3, 2024)  {
     val regex = Regex("(mul\\(\\d+,\\d+\\))")
-    override fun part1(): Any = regex.findAll(loadToString()).sumOf { mul ->
-        mul.value.replace("mul(", "").replace(")", "").split(",").let { (a, b) -> a.toInt() * b.toInt() }
+    val dontRegex = Regex("don't\\(\\)(.|\\n)*?do\\(\\)")
+
+    override fun part1(): Any = doPartOne(loadToString())
+
+    // 159892596
+    private fun doPartOne(string: String) = regex.findAll(string).sumOf { mul ->
+            mul.value.replace("mul(", "").replace(")", "").split(",").let { (a, b) -> a.toInt() * b.toInt() }
+        }
+
+    // substring to remove any don't that might be at the end without a do
+    // 92626942
+    override fun part2(): Any = dontRegex.replace(loadToString(testNum=2)) {""}.substringBefore("don't()").let {
+        doPartOne(it)
     }
 }
