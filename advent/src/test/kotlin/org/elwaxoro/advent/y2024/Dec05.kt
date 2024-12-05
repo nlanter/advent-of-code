04c88a57a2c31495a1b9c645488ba1cea98f939a
nlanter <nlanter@figure.com>
2024-12-05 16:26:15 -0500

    d5p2

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec05.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec05.kt
@@ -2,10 +2,34 @@ package org.elwaxoro.advent.y2024
 
 import org.elwaxoro.advent.PuzzleDayTester
 
-class Dec5: PuzzleDayTester(5, 2024)  {
+class Dec5 : PuzzleDayTester(5, 2024) {
 
     //5955
-    override fun part1(): Any = loadToString().split("\n\n").let { (rules, pages) ->
+    override fun part1(): Any = splitGoodAndBad().first.sumOf {
+        it[it.size / 2]
+    }
+
+    override fun part2(): Any {
+        val rules = getRulesAndPages(testNum = 2).first
+        return splitGoodAndBad(testNum = 2).second.map {
+            it.sortedWith { a, b ->
+                if (rules[a.toInt()]?.contains(b.toInt()) == true) -1 else 0
+            }
+        }.sumOf {
+            it[it.size / 2]
+        }
+    }
+
+    private fun splitGoodAndBad(testNum: Int? = null): Pair<List<List<Int>>, List<List<Int>>> = getRulesAndPages(testNum = testNum).let { (rules, pages) ->
+        pages.split("\n").map { it.split(",").map { it.toInt() } }.partition { line ->
+            line.mapIndexed { idx, str ->
+                line.drop(idx).none { rules[str]?.contains(it) == true }
+            }.all { it }
+        }
+    }
+
+
+    private fun getRulesAndPages(testNum: Int? = null): Pair<MutableMap<Int, MutableList<Int>>, String> = loadToString(testNum = testNum).split("\n\n").let { (rules, pages) ->
         val ruleMap = mutableMapOf<Int, MutableList<Int>>() // list of everything which must be before that number
         rules.split("\n").onEach {
             val split = it.split("|")
@@ -15,14 +39,6 @@ class Dec5: PuzzleDayTester(5, 2024)  {
                 ruleMap[split[1].toInt()] = mutableListOf(split[0].toInt())
             }
         }
-
-        pages.split("\n").map { it.split(",") }.filter { line ->
-            line.mapIndexed { idx, str ->
-                line.drop(idx).none { ruleMap[str.toInt()]?.contains(it.toInt()) == true}
-            }.all { it }
-        }.sumOf {
-            it[it.size / 2].toInt()
-        }
-
+        return ruleMap to pages
     }
 }
